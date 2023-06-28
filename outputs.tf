9b8e0dc81b5afea08aba917cf568bf4d15a5d3ef
Vadim Sohin , DevOps  Engineer <vadim.sohin@gmail.com>
2023-06-28 19:30:00 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,12 @@
-
 output "resource_group_name" {
   value = azurerm_resource_group.rg1.name
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet1
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sg
+  sensitive = true
 }
